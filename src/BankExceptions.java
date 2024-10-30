c02379eca76eb6079aa457e43f082ce5740a7e74
Jess-Do <jdo5@students.kennesaw.edu>
2024-10-30 15:00:34 -0400

    added comments to exception class

--- src/BankExceptions.java
+++ src/BankExceptions.java
@@ -1,25 +1,31 @@
+// class to hold all the bank exceptions
 public class BankExceptions {
+    // base constructor
     public BankExceptions(String message) {
     }
 
+    // invalid password exception
     public static class InvalidPasswordFormatException extends Exception {
         public InvalidPasswordFormatException(String message) {
             super(message);
         }
     }
 
+    // negative dollar ammount exception
     public class NegativeDollarAmountException extends Exception {
         public NegativeDollarAmountException(String message) {
             super(message);
         }
     }
 
+    // insufficient funds exceptions
     public class InsufficientFundsException extends Exception {
         public InsufficientFundsException(String message) {
             super(message);
         }
     }
 
+    // unable to find customer account exception
     public class CustomerAccountNotFoundException extends Exception {
         public CustomerAccountNotFoundException(String message) {
             super(message);
