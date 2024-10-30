157acaff61d0f3d1365b384c854f51cede33d0bf
Jess-Do <jdo5@students.kennesaw.edu>
2024-10-30 14:56:53 -0400

    added comments

--- src/Checking.java
+++ src/Checking.java
@@ -1,12 +1,23 @@
+// checking class extends/inherits Bank_Account class
 public class Checking extends BankAccount{
 
+    // daily withdrawal limit
     private double dailyWithdrawalLimit;
 
+    // overloaded constructor for checking + throws exception if the password format is invalid
     public Checking(String customerFullName, String userID, String password) throws BankExceptions.InvalidPasswordFormatException {
+        // calling bank_account constructor
         super(customerFullName, userID, password);
+        // the limit
         this.dailyWithdrawalLimit = 300;
     }
-
-    public double getDailyWithdrawalLimit() { return dailyWithdrawalLimit; }
-    public void setDailyWithdrawalLimit(double limit) { this.dailyWithdrawalLimit = limit; }
+    
+    // getting the daily withdrawal limit
+    public double getDailyWithdrawalLimit() { 
+        return dailyWithdrawalLimit; 
+    }
+    // setting the daily withdrawal limit
+    public void setDailyWithdrawalLimit(double limit) { 
+        this.dailyWithdrawalLimit = limit; 
+    }
 }
